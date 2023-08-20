faf8a3d1f196c1e238dff38dc6ae6946face1226
kvucicevic <kvucicevic5721rn@raf.rs>
2023-08-20 13:01:21 +0200

    finished the rest of the parts of structures task from Jan1819

--- Jan1819/strukture1.c
+++ Jan1819/strukture1.c
@@ -10,9 +10,9 @@
             uređenu nerastude po broju rođenih beba,
             b) (8p) ispis broja rođenih beba u jednoj godini, unosi se godina i treba ispisati broj rođenih u toj
             godini, ovu funkcionalnost je potrebno implementirati na dva načina, iterativno i rekurzivno,
-            c) (10p) ispis broja rođenih beba po mesecima sortirano rastude po godini i mesecu, posmatra se
+            c) (10p) ispis broja rođenih beba po mesecima sortirano rastuce po godini i mesecu, posmatra se
             broj rođenih beba u konkretnom mesecu (na primer 2.2018.) i ispisuje se mesec, godina i broj
-            rođenih u tom mesecu, sortirano rastude po mesecu (dozvoljeno je praviti nove strukture podataka)
+            rođenih u tom mesecu, sortirano rastuce po mesecu (dozvoljeno je praviti nove strukture podataka)
  */
 
 typedef struct Porodiliste Porodiliste;
@@ -26,13 +26,15 @@ struct Porodiliste {
 void insertionSort(Porodiliste** head);
 void sortedInsert(Porodiliste** head, Porodiliste* newNode);
 void printList(Porodiliste* head);
+void ispisiRekurzivno(Porodiliste* head, int godina, int count);
+void printChronologicaly(Porodiliste* head);
 
-void loadFile(char* fileName){
+Porodiliste* loadFile(char* fileName){
 
     FILE *file = fopen(fileName, "r");
     if (file == NULL) {
         perror("Error opening file");
-        return;
+        return NULL;
     }
 
     Porodiliste* head = NULL;
@@ -44,7 +46,7 @@ void loadFile(char* fileName){
         int dan;
         int brojBeba;
 
-        sscanf(line, "%d %d %d %d ", &godina, &mesec, &dan, &brojBeba);
+        sscanf(line, "%d %d %d %d ", &dan, &mesec, &godina, &brojBeba);
         Porodiliste* node = (Porodiliste*)malloc(sizeof(Porodiliste));
         node->godina = godina;
         node->mesec = mesec;
@@ -63,6 +65,7 @@ void loadFile(char* fileName){
 
     insertionSort(&head);
     printList(head);
+    return head;
 
 }
 
@@ -100,10 +103,120 @@ void insertionSort(Porodiliste** headRef){
     *headRef = sorted;
 }
 
+void ispisPoGodini(Porodiliste* head, int godina){
+    Porodiliste* current = head;
+    int count = 0;
+    while(current->nextB != NULL){
+        if(current->godina == godina){
+            //printf("Datum: %d-%d-%d, Broj beba: %d\n", current->dan, current->mesec, current->godina, current->brojBeba);
+            count++;
+        }
+        current = current->nextB;
+    }
+    printf("broj rodjenih beba u godini: %d je: %d\n", godina, count);
+
+}
+
+void ispisiRekurzivno(Porodiliste* head, int godina, int count){
+
+    if(head == NULL){
+        printf("no is: %d\n", count);
+        return;
+    }
+
+    if(godina == head->godina){
+        ispisiRekurzivno(head->nextB, godina, count+1);
+    } else {
+        ispisiRekurzivno(head->nextB, godina, count);
+    }
+
+}
+
+int getIthSmallestYear(Porodiliste* head, int i) {
+    Porodiliste* current = head;
+    int smallestYears[i]; // An array to store the i smallest years
+    for (int j = 0; j < i; j++) {
+        smallestYears[j] = current->godina;
+        current = current->nextB;
+    }
+
+    //insertionSort(&smallestYears); // Sort the array of smallest years
+
+    return smallestYears[i - 1]; // Return the i-th smallest year
+}
+
+void sortChronologiclaly(Porodiliste** headRef, Porodiliste* newNode) {
+    Porodiliste* current;
+
+    if (*headRef == NULL || (*headRef)->godina > newNode->godina ||
+        ((*headRef)->godina == newNode->godina && (*headRef)->mesec >= newNode->mesec)) {
+        newNode->nextB = *headRef;
+        *headRef = newNode;
+    } else {
+        current = *headRef;
+        while (current->nextB != NULL &&
+               (current->nextB->godina < newNode->godina ||
+                (current->nextB->godina == newNode->godina && current->nextB->mesec < newNode->mesec))) {
+            current = current->nextB;
+        }
+        newNode->nextB = current->nextB;
+        current->nextB = newNode;
+    }
+}
+
+void printSorted(Porodiliste** headRef) {
+    Porodiliste* sorted = NULL;
+    Porodiliste* current = *headRef;
+
+    while (current != NULL) {
+        Porodiliste* next = current->nextB;
+        sortChronologiclaly(&sorted, current);
+        current = next;
+    }
+
+    *headRef = sorted;
+    printChronologicaly(*headRef);
+}
+
+
+void printChronologicaly(Porodiliste* head){
+    Porodiliste* current = head;
+    int currentMonth = 1;
+    int count = 0;
+    while(current !=  NULL){
+        Porodiliste* next = current->nextB;
+
+        if(current->mesec == currentMonth){
+            count++;
+        } else {
+            printf("Year: %d Month: %d No: %d\n", current->godina, currentMonth, count);
+            count = 1;
+            currentMonth = current->mesec;
+        }
+
+        current = next;
+    }
+}
 
 int main(){
 
-    loadFile("grupa1.txt");
+    Porodiliste* head = loadFile("grupa1.txt");
+
+    int choice;
+    scanf("%d", &choice);
+
+    if(choice == 1){
+        int godina;
+        scanf("%d", &godina);
+        ispisPoGodini(head, godina);
+    } else if (choice == 2){
+        int godina;
+        scanf("%d", &godina);
+        Porodiliste* current = head;
+        ispisiRekurzivno(current, godina, 0);
+    } else {
+        printSorted(&head);
+    }
 
     return 0;
 }
